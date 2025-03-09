d1fb617125f1e65e5ae59f0bea33ce06f4b87a54
Igor Barcovsky <igor@barcovsky.ru>
2025-03-09 18:29:53 +0400

    Add InputFile yet again because I was stupid last time

--- bot.py
+++ bot.py
@@ -14,7 +14,8 @@ from apscheduler.schedulers.asyncio import AsyncIOScheduler
 from apscheduler.triggers.cron import CronTrigger
 import os
 from io import BytesIO
-import InputFile
+from aiogram.types import InputFile
+
 
 # Глобальная переменная для хранения начального лимита на месяц
 initial_budget = None
